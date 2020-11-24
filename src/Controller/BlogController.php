d566646ce9f8c8c8eb87cedd65ed3abe44e3a432
asugi <road.83@hotmail.fr>
2020-11-24 08:16:35 +0100

    last

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,18 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index(): Response
+    public function index(ArticleRepository $repo)
     {
+        $articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
 
@@ -25,10 +29,42 @@ class BlogController extends AbstractController
         return $this->render('blog/home.html.twig');
     }
 
-        /**
-     * @Route("/blog/12", name="blog_show")
+    /**
+     * @Route("/blog/new", name="blog_create")
      */
-    public function show(){
-        return $this->render('blog/show.html.twig');
+    public function create(){
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+                     ->add('title')
+                     ->add('content')
+                     ->add('image')
+                     ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $article->setCreatedAt(new \DateTime());
+
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView()
+        ]);
     }
+
+    /**
+     * @Route("/blog/{id}", name="blog_show")
+     */
+    public function show(Article $article){
+
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+        ]);
+    }
+
 }
